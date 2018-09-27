07ad1a48c6d4d16395a05bd18f64c11feecde94c
tamminen.mikkoj <tamminen.mikkoj@gmail.com>
2018-09-27 06:44:17 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,13 +2,3 @@ def reverse_each_word(sentence)
   sentence.split.collect {|word| word.reverse}.join(" ")
 end
 
-
-## def reverse_each_word(string)
-  new_array = string.split(" ")
-  new_array.each do |word|
-    puts word.reverse
-  end
-    array_reversed = array.reverse
-    puts array_reversed
-  end
-end
